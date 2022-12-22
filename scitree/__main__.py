8f7928f0df95e61a5aac355a7b8d4a82fc4a6bed
JT <j.j.teijema@uu.nl>
2022-12-22 17:45:32 +0100

    Add arguments to main

--- scitree/__main__.py
+++ scitree/__main__.py
@@ -27,6 +27,11 @@ def main():
         action="store_true",
         help="Show files and folders from gitignore file.",
     )
+    parser.add_argument(
+        "-e",
+        action="store_true",
+        help="Add icons to files and folders.",
+    )
     # version
     parser.add_argument(
         "-V",
@@ -44,6 +49,8 @@ def main():
 
     if not args.a:
         options["exclude_folders"] = [".git"]
+
+    options["icons"] = args.e
 
     scitree(args.dir, **options)
 
